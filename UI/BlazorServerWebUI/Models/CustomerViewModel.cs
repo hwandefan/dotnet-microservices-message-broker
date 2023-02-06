23ec0a260ffc31c6c4b002f80d29d629ffdf8b79
Alexandr Sokolov <hwandefan@gmail.com>
2023-02-06 02:43:13 +0100

    Routing UI on services

---
+++ UI/BlazorServerWebUI/Models/CustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorServerWebUI.Models
+{
+	public class CustomerViewModel
+	{
+        public int Customer_Id { get; set; }
+        public string CustomerName { get; set; }
+        public string MobileNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
+
---
+++ UI/BlazorServerWebUI/Models/OrderViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorServerWebUI.Models
+{
+	public class OrderViewModel
+	{
+        public int Order_Id { get; set; }
+        public string Name { get; set; }
+        public string Customer_Id { get; set; }
+    }
+}
+
